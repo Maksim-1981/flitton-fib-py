4291111f3d71b723f1e2bbd184363a5787409b69
HomeMac <homemac@Mac-mini-HomeMac.local>
2024-12-11 16:47:06 +0300

    addition setup to module

---
+++ packt_fibonacci/__init__.py
@@ -0,0 +1,2 @@
+def say_hello()->None:
+    print("the flitton Fibonacci module is saying hello")
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+with open("README.md", "r") as fh:
+long_description = fh.read()
 setup(
 name="flitton_fib_py",
 version="0.0.1",
 author="Maxwell Flitton",
 author_email="maxwell@gmail.com",
 description="Calculates a Fibonacci number",
-long_description="A basic library that calculates Fibonacci numbers",
+long_description=long_description,
 long_description_content_type="text/markdown",
 url="https://github.com/Maksim-1981/flitton-fib-py",
 install_requires=[],
