e0b1327944c95f523b82fd03e48090ba12ebd6e6
HomeMac <homemac@Mac-mini-HomeMac.local>
2024-12-13 09:39:51 +0300

    addition setup to module

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
     name="flitton_fib_py",
     version="0.0.1",
     author="Maxwell Flitton",
     author_email="maxwell@gmail.com",
     description="Calculates a Fibonacci number",
-    long_description="long_description",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/maxwellflitton/flitton-fib-py",
+    url="https://github.com/Maksim-1981/flitton-fib-py",
     install_requires=[],
     packages=find_packages(exclude=("tests",)),
     classifiers=[
