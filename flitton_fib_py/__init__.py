a057283aa2fd69d5dd7b41ef7a4f3217c88c5615
Maksim-1981 <97602604+Maksim-1981@users.noreply.github.com>
2025-01-26 18:38:20 +0300

    adding setup to module

--- flitton_fib_py/__init__.py
+++
@@ -1,2 +0,0 @@
-def say_helo()->None:
-    print ('the Flitton Fibonacci module is saying hello')
---
+++ flitton_fib_py/_init_.py
@@ -0,0 +1,4 @@
+
+
+def say_helo() -> None:
+    print ("the Flitton Fibonacci module is saying hello")
