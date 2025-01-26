2cb927c881517b887b3987e337b962bed435b785
Maksim-1981 <97602604+Maksim-1981@users.noreply.github.com>
2025-01-26 18:55:23 +0300

    _init_.py

--- flitton_fib_py/_init_.py
+++ flitton_fib_py/_init_.py
@@ -1,5 +1,4 @@
 
 
 def say_helo() -> None:
-    
-                                  print("the Flitton Fibonacci module is saying hello")
+    print("the Flitton Fibonacci module is saying hello"),
