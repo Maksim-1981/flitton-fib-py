0dd1de9c5392f5089e140a0585c7f422dd4b1e14
Maksim-1981 <97602604+Maksim-1981@users.noreply.github.com>
2025-03-02 17:12:00 +0300

    addition setup to module

--- flitton_fib_py/cmd/fib_numb.py
+++ flitton_fib_py/cmd/fib_numb.py
@@ -7,8 +7,6 @@ def fib_numb() -> None:
         description='Calculate Fibonacci numbers')
 
 
-parser.add_argument('--number', action='store',
-                    type=int, required=True,
-                    help="Fibonacci number to be calculated")
+parser.add_argument('--number', action='store', type=int, required=True, help="Fibonacci number to be calculated")
 args = parser.parse_args()
 print(f"Your Fibonacci number is: " f"{recurring_fibonacci_number(number=args.number)}")
